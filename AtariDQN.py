b1995b315188940c838ef8f1e919ada46dc95321
Jesper Derehag <jesper.derehag@ericsson.com>
2016-11-01 14:22:35 +0100

    Initial tensorboard support
    Signed-off-by: Jesper Derehag <jesper.derehag@ericsson.com>

--- AtariDQN.py
+++ AtariDQN.py
@@ -1,43 +1,52 @@
-#!//usr/bin/env python
+#!/usr/bin/env python
+import os
+import argparse
 
 import cv2
-import sys
 from game.Atari import Atari
-from BrainDQN_Nature import *
+from BrainDQN_Nature import BrainDQN
 import numpy as np
 
 # preprocess raw image to 80*80 gray image
 def preprocess(observation):
     observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
-    observation = observation[26:110,:]
-    ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
-    return np.reshape(observation,(84,84,1))
+    observation = observation[26:110, :]
+    _, observation = cv2.threshold(observation, 1, 255, cv2.THRESH_BINARY)
+    return np.reshape(observation, (84, 84, 1))
 
-def playAtari():
+def playAtari(args):
     # Step 1: init BrainDQN
     # Step 2: init Flappy Bird Game
     atari = Atari('breakout.bin')
     actions = len(atari.legal_actions)
-    brain = BrainDQN(actions)
+    brain = BrainDQN(actions, args.checkpoints, args.summary)
 
     # Step 3: play game
     # Step 3.1: obtain init state
-    action0 = np.array([1,0,0,0])  # do nothing
+    action0 = np.array([1, 0, 0, 0])  # do nothing
     observation0, reward0, terminal = atari.next(action0)
     observation0 = cv2.cvtColor(cv2.resize(observation0, (84, 110)), cv2.COLOR_BGR2GRAY)
-    observation0 = observation0[26:110,:]
-    ret, observation0 = cv2.threshold(observation0,1,255,cv2.THRESH_BINARY)
+    observation0 = observation0[26:110, :]
+    _, observation0 = cv2.threshold(observation0, 1, 255, cv2.THRESH_BINARY)
     brain.setInitState(observation0)
 
     # Step 3.2: run the game
     while True:
         action = brain.getAction()
-        nextObservation,reward,terminal = atari.next(action)
+        nextObservation, reward, terminal = atari.next(action)
         nextObservation = preprocess(nextObservation)
-        brain.setPerception(nextObservation,action,reward,terminal)
+        brain.setPerception(nextObservation, action, reward, terminal)
 
 def main():
-    playAtari()
+    parser = argparse.ArgumentParser()
+    data_basepath = 'data'
+    parser.add_argument('-c', '--checkpoints', dest='checkpoints', default=os.path.join(data_basepath, 'checkpoints'),
+                        help='Path where to store checkpoints (i.e partial training)')
+    parser.add_argument('-s', '--summary', dest='summary', default=os.path.join(data_basepath, 'summary'),
+                        help='Path where to store summary data (for tensorboard)')
+    args = parser.parse_args()
+
+    playAtari(args)
 
 if __name__ == '__main__':
     main()
--- BrainDQN_Nature.py
+++ BrainDQN_Nature.py
@@ -3,103 +3,156 @@
 # Author: Flood Sung
 # Date: 2016.3.21
 # -----------------------------
-
-import tensorflow as tf
-import numpy as np
+import os
+import time
+import datetime
 import random
 from collections import deque
 
+import tensorflow as tf
+import numpy as np
+
 # Hyper Parameters:
 FRAME_PER_ACTION = 1
-GAMMA = 0.95 # decay rate of past observations
-OBSERVE = 50000. # timesteps to observe before training
-EXPLORE = 1000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.1#0.001 # final value of epsilon
-INITIAL_EPSILON = 1.0#0.01 # starting value of epsilon
-REPLAY_MEMORY = 1000000 # number of previous transitions to remember
-BATCH_SIZE = 32 # size of minibatch
+GAMMA = 0.95  # decay rate of past observations
+OBSERVE = 50000.  # timesteps to observe before training
+EXPLORE = 1000000.  # frames over which to anneal epsilon
+FINAL_EPSILON = 0.1  # 0.001 # final value of epsilon
+INITIAL_EPSILON = 1.0  # 0.01 # starting value of epsilon
+REPLAY_MEMORY = 1000000  # number of previous transitions to remember
+BATCH_SIZE = 32  # size of minibatch
 UPDATE_TIME = 10000
 
 class BrainDQN:
-    def __init__(self,actions):
-        # init replay memory
+    def __init__(self, actions, checkpointpath=None, summarypath=None):
         self.replayMemory = deque()
-        # init some parameters
+
         self.timeStep = 0
-        self.epsilon = INITIAL_EPSILON
+        self.prevTimeStep = 0
+        self.prevTimeStamp = time.clock()
+        self.fps = tf.Variable(initial_value=0.0, dtype=tf.float32, name='fps')
+        self.meta_state = tf.Variable(initial_value=0, dtype=tf.uint8, name='meta_state')  # (0-observe, 1-explore, 2-train)
+        self.epsilon = tf.Variable(initial_value=INITIAL_EPSILON, dtype=tf.float32, name='epsilon')
+
         self.actions = actions
+
         # init Q network
-        self.stateInput,self.QValue,self.W_conv1,self.b_conv1,self.W_conv2,self.b_conv2,self.W_conv3,self.b_conv3,self.W_fc1,self.b_fc1,self.W_fc2,self.b_fc2 = self.createQNetwork()
+        self.stateInput, \
+        self.QValue, \
+        self.W_conv1, \
+        self.b_conv1, \
+        self.W_conv2, \
+        self.b_conv2, \
+        self.W_conv3, \
+        self.b_conv3, \
+        self.W_fc1, \
+        self.b_fc1, \
+        self.W_fc2, \
+        self.b_fc2 = self.createQNetwork()
 
         # init Target Q Network
-        self.stateInputT,self.QValueT,self.W_conv1T,self.b_conv1T,self.W_conv2T,self.b_conv2T,self.W_conv3T,self.b_conv3T,self.W_fc1T,self.b_fc1T,self.W_fc2T,self.b_fc2T = self.createQNetwork()
+        self.stateInputT, \
+        self.QValueT, \
+        self.W_conv1T, \
+        self.b_conv1T, \
+        self.W_conv2T, \
+        self.b_conv2T, \
+        self.W_conv3T, \
+        self.b_conv3T, \
+        self.W_fc1T, \
+        self.b_fc1T, \
+        self.W_fc2T, \
+        self.b_fc2T = self.createQNetwork()
 
-        self.copyTargetQNetworkOperation = [self.W_conv1T.assign(self.W_conv1),self.b_conv1T.assign(self.b_conv1),self.W_conv2T.assign(self.W_conv2),self.b_conv2T.assign(self.b_conv2),self.W_conv3T.assign(self.W_conv3),self.b_conv3T.assign(self.b_conv3),self.W_fc1T.assign(self.W_fc1),self.b_fc1T.assign(self.b_fc1),self.W_fc2T.assign(self.W_fc2),self.b_fc2T.assign(self.b_fc2)]
+        self.copyTargetQNetworkOperation = [self.W_conv1T.assign(self.W_conv1),
+                                            self.b_conv1T.assign(self.b_conv1),
+                                            self.W_conv2T.assign(self.W_conv2),
+                                            self.b_conv2T.assign(self.b_conv2),
+                                            self.W_conv3T.assign(self.W_conv3),
+                                            self.b_conv3T.assign(self.b_conv3),
+                                            self.W_fc1T.assign(self.W_fc1),
+                                            self.b_fc1T.assign(self.b_fc1),
+                                            self.W_fc2T.assign(self.W_fc2),
+                                            self.b_fc2T.assign(self.b_fc2)]
 
         self.createTrainingMethod()
 
+        self.session = tf.InteractiveSession()
+
+        if summarypath is not None:
+            tf.scalar_summary(r'overview/meta_state (0-observe, 1-explore, 2-train)', self.meta_state)
+            tf.scalar_summary(r'overview/epsilon (exploration probability)', self.epsilon)
+            tf.scalar_summary(r'performance/frames_per_second', self.fps)
+            self.summaries = tf.merge_all_summaries()
+            self.summarywriter = tf.train.SummaryWriter(os.path.join(summarypath, str(datetime.datetime.now())), self.session.graph)
+
         # saving and loading networks
         self.saver = tf.train.Saver()
-        self.session = tf.InteractiveSession()
         self.session.run(tf.initialize_all_variables())
-        checkpoint = tf.train.get_checkpoint_state("saved_networks")
-        if checkpoint and checkpoint.model_checkpoint_path:
-            self.saver.restore(self.session, checkpoint.model_checkpoint_path)
-            print "Successfully loaded:", checkpoint.model_checkpoint_path
-        else:
-            print "Could not find old network weights"
 
+        if checkpointpath is not None:
+            try:
+                os.makedirs(checkpointpath)
+            except OSError:
+                pass
+
+            self.checkpoint = tf.train.get_checkpoint_state(checkpointpath)
+            if self.checkpoint and self.checkpoint.model_checkpoint_path:
+                self.saver.restore(self.session, self.checkpoint.model_checkpoint_path)
+                print "Successfully loaded:", self.checkpoint.model_checkpoint_path
+            else:
+                print "Could not find old network weights"
 
     def createQNetwork(self):
         # network weights
-        W_conv1 = self.weight_variable([8,8,4,32])
+        W_conv1 = self.weight_variable([8, 8, 4, 32])
         b_conv1 = self.bias_variable([32])
 
-        W_conv2 = self.weight_variable([4,4,32,64])
+        W_conv2 = self.weight_variable([4, 4, 32, 64])
         b_conv2 = self.bias_variable([64])
 
-        W_conv3 = self.weight_variable([3,3,64,64])
+        W_conv3 = self.weight_variable([3, 3, 64, 64])
         b_conv3 = self.bias_variable([64])
 
-        W_fc1 = self.weight_variable([3136,512])
+        W_fc1 = self.weight_variable([3136, 512])
         b_fc1 = self.bias_variable([512])
 
-        W_fc2 = self.weight_variable([512,self.actions])
+        W_fc2 = self.weight_variable([512, self.actions])
         b_fc2 = self.bias_variable([self.actions])
 
         # input layer
-        stateInput = tf.placeholder("float",[None,84,84,4])
+        stateInput = tf.placeholder("float", [None, 84, 84, 4])
 
         # hidden layers
-        h_conv1 = tf.nn.relu(self.conv2d(stateInput,W_conv1,4) + b_conv1)
-        #h_pool1 = self.max_pool_2x2(h_conv1)
+        h_conv1 = tf.nn.relu(self.conv2d(stateInput, W_conv1, 4) + b_conv1)
+        # h_pool1 = self.max_pool_2x2(h_conv1)
 
-        h_conv2 = tf.nn.relu(self.conv2d(h_conv1,W_conv2,2) + b_conv2)
+        h_conv2 = tf.nn.relu(self.conv2d(h_conv1, W_conv2, 2) + b_conv2)
 
-        h_conv3 = tf.nn.relu(self.conv2d(h_conv2,W_conv3,1) + b_conv3)
-        h_conv3_shape = h_conv3.get_shape().as_list()
-        print "dimension:",h_conv3_shape[1]*h_conv3_shape[2]*h_conv3_shape[3]
-        h_conv3_flat = tf.reshape(h_conv3,[-1,3136])
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat,W_fc1) + b_fc1)
+        h_conv3 = tf.nn.relu(self.conv2d(h_conv2, W_conv3, 1) + b_conv3)
+        # h_conv3_shape = h_conv3.get_shape().as_list()
+        # print "dimension:", h_conv3_shape[1] * h_conv3_shape[2] * h_conv3_shape[3]
+        h_conv3_flat = tf.reshape(h_conv3, [-1, 3136])
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
         # Q Value layer
-        QValue = tf.matmul(h_fc1,W_fc2) + b_fc2
+        QValue = tf.matmul(h_fc1, W_fc2) + b_fc2
 
-        return stateInput,QValue,W_conv1,b_conv1,W_conv2,b_conv2,W_conv3,b_conv3,W_fc1,b_fc1,W_fc2,b_fc2
+        return stateInput, QValue, W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W_fc1, b_fc1, W_fc2, b_fc2
 
     def copyTargetQNetwork(self):
         self.session.run(self.copyTargetQNetworkOperation)
 
     def createTrainingMethod(self):
-        self.actionInput = tf.placeholder("float",[None,self.actions])
+        self.actionInput = tf.placeholder("float", [None, self.actions])
         self.yInput = tf.placeholder("float", [None])
-        Q_Action = tf.reduce_sum(tf.mul(self.QValue, self.actionInput), reduction_indices = 1)
+        Q_Action = tf.reduce_sum(tf.mul(self.QValue, self.actionInput), reduction_indices=1)
         self.cost = tf.reduce_mean(tf.square(self.yInput - Q_Action))
-        self.trainStep = tf.train.RMSPropOptimizer(0.00025,0.99,0.0,1e-6).minimize(self.cost)
+        self.trainStep = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6).minimize(self.cost)
 
     def trainQNetwork(self):
         # Step 1: obtain random minibatch from replay memory
-        minibatch = random.sample(self.replayMemory,BATCH_SIZE)
+        minibatch = random.sample(self.replayMemory, BATCH_SIZE)
         state_batch = [data[0] for data in minibatch]
         action_batch = [data[1] for data in minibatch]
         reward_batch = [data[2] for data in minibatch]
@@ -108,7 +161,7 @@ class BrainDQN:
         # Step 2: calculate y
         y_batch = []
         QValue_batch = self.QValueT.eval(feed_dict={self.stateInputT:nextState_batch})
-        for i in range(0,BATCH_SIZE):
+        for i in range(0, BATCH_SIZE):
             terminal = minibatch[i][4]
             if terminal:
                 y_batch.append(reward_batch[i])
@@ -120,69 +173,80 @@ class BrainDQN:
                                       self.stateInput : state_batch})
 
         # save network every 100000 iteration
-        if self.timeStep % 10000 == 0:
-            self.saver.save(self.session, 'saved_networks/' + 'network' + '-dqn', global_step = self.timeStep)
+        if self.timeStep % 100000 == 0:
+            self.saver.save(self.session, 'dqn', global_step=self.timeStep)
 
         if self.timeStep % UPDATE_TIME == 0:
             self.copyTargetQNetwork()
 
-    def setPerception(self,nextObservation,action,reward,terminal):
-        newState = np.append(nextObservation,self.currentState[:,:,1:],axis = 2)
-        self.replayMemory.append((self.currentState,action,reward,newState,terminal))
+    def setPerception(self, nextObservation, action, reward, terminal):
+        newState = np.append(nextObservation, self.currentState[:, :, 1:], axis=2)
+        self.replayMemory.append((self.currentState, action, reward, newState, terminal))
         if len(self.replayMemory) > REPLAY_MEMORY:
             self.replayMemory.popleft()
         if self.timeStep > OBSERVE:
             # Train the network
             self.trainQNetwork()
 
-        # print info
-        state = ""
-        if self.timeStep <= OBSERVE:
-            state = "observe"
-        elif self.timeStep > OBSERVE and self.timeStep <= OBSERVE + EXPLORE:
-            state = "explore"
-        else:
-            state = "train"
-
-        print "TIMESTEP", self.timeStep, "/ STATE", state, \
-        "/ EPSILON", self.epsilon
+        if self.timeStep % 100 == 0:
+            self._write_counters()
 
         self.currentState = newState
         self.timeStep += 1
 
     def getAction(self):
-        QValue = self.QValue.eval(feed_dict= {self.stateInput:[self.currentState]})[0]
+        QValue = self.QValue.eval(feed_dict={self.stateInput:[self.currentState]})[0]
         action = np.zeros(self.actions)
         action_index = 0
+        epsilon = self.epsilon.eval()
         if self.timeStep % FRAME_PER_ACTION == 0:
-            if random.random() <= self.epsilon:
+            if random.random() <= epsilon:
                 action_index = random.randrange(self.actions)
                 action[action_index] = 1
             else:
                 action_index = np.argmax(QValue)
                 action[action_index] = 1
         else:
-            action[0] = 1 # do nothing
+            action[0] = 1  # do nothing
 
         # change episilon
-        if self.epsilon > FINAL_EPSILON and self.timeStep > OBSERVE:
-            self.epsilon -= (INITIAL_EPSILON - FINAL_EPSILON)/EXPLORE
+        if epsilon > FINAL_EPSILON and self.timeStep > OBSERVE:
+            self.epsilon.assign_sub((INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE).run()
 
         return action
 
-    def setInitState(self,observation):
-        self.currentState = np.stack((observation, observation, observation, observation), axis = 2)
+    def setInitState(self, observation):
+        self.currentState = np.stack((observation, observation, observation, observation), axis=2)
 
-    def weight_variable(self,shape):
-        initial = tf.truncated_normal(shape, stddev = 0.01)
+    def _write_counters(self):
+        # FPS
+        new_timestamp = time.clock()
+        self.fps.assign((self.timeStep - self.prevTimeStep) / (new_timestamp - self.prevTimeStamp)).op.run()
+        self.prevTimeStamp = new_timestamp
+        self.prevTimeStep = self.timeStep
+
+        # meta state (0-observe, 1-explore, 2-train)
+        if self.timeStep <= OBSERVE:
+            state = 0
+        elif self.timeStep > OBSERVE and self.timeStep <= OBSERVE + EXPLORE:
+            state = 1
+        else:
+            state = 2
+        self.meta_state.assign(state).op.run()
+
+        summary = self.session.run(self.summaries)
+        self.summarywriter.add_summary(summary, self.timeStep)
+
+    def weight_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=0.01)
         return tf.Variable(initial)
 
-    def bias_variable(self,shape):
-        initial = tf.constant(0.01, shape = shape)
+    def bias_variable(self, shape):
+        initial = tf.constant(0.01, shape=shape)
         return tf.Variable(initial)
 
-    def conv2d(self,x, W, stride):
-        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "VALID")
+    def conv2d(self, x, W, stride):
+        return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="VALID")
 
-    def max_pool_2x2(self,x):
-        return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
